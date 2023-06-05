ee3827707534bc181ec18041f44869a594244bfd
segonz <segonz@bancolombia.com.co>
2023-06-04 21:14:47 -0500

    Se crea boot de aplicación y libreria para envio a email

---
+++ boot.py
@@ -0,0 +1,29 @@
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'Samantha'
+password = '43440492'
+
+station = network.WLAN(network.STA_IF)
+station.active(False)
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+relay = Pin(26, Pin.OUT)
